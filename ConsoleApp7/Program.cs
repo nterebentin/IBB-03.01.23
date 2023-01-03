d5a2ec0b9a97e3e6fcb5356d5bedae305755c5c7
Nursen Özcan <nursenozcan95@gmail.com>
2023-01-03 17:34:36 +0300

    Add project files.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+    class DataTable: IEnumerable<DataRow>, IEnumerator<DataRow>
+    {
+        List<DataRow> rows = new List<DataRow>();
+
+        public DataRow Current => rows[index];
+
+        object IEnumerator.Current => throw new NotImplementedException();
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<DataRow> GetEnumerator()
+        {
+            return this;
+        }
+
+        int index = -1;
+        public bool MoveNext()
+        {
+            if (index< rows.Count - 1)
+            {
+                index++;
+                return true;
+            }
+            else
+            { return false; }
+        }
+
+        public void Reset()
+        {
+            index=-1;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class DataRow :IEnumerable, IEnumerator
+    {
+        List<object> values = new List<object>();
+        public object this[int i]
+        {
+            get { return values[i]; }
+        }
+
+       
+        public IEnumerator GetEnumerator()
+        {
+            return this;
+        }
+
+        int index = -1;
+        public bool MoveNext()
+        {
+            if(index < values.Count - 1)
+            {
+                index++;
+                return true;
+            }
+            else { return false; }  
+        }
+        
+
+        public void Reset()
+        {
+            index = -1;
+        }
+        public object Current => values[index];
+}
+    }
+    
